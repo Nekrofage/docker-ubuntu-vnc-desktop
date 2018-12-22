5e07756ad5ae609c25bf3b111f8bb74f28c1f72a
Nekrofage <lesanglierdesardennes@gmail.com>
2018-12-22 23:14:20 +0100

    Define Volume as user home directory

--- runDockerImage.sh
+++ runDockerImage.sh
@@ -4,7 +4,7 @@ docker run -p 6080:80 \
 	-e RESOLUTION=1024x768 \
         -e USER=util01 -e PASSWORD=mot2passe \
         -e ALSADEV=hw:2,0 \
-	-v volume01:/volume/volume01 \
+	-v volume01:/home/util01 \
         --device /dev/snd \
 	--hostname=container01 \
 	nekrofage/ubuntu-desktop-lxde-vnc-zandronum:latest
