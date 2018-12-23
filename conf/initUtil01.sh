afa13583e83a9483a4429b7747a5a9a7a9069e09
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2018-12-23 23:51:22 +0100

    Update Zandonum install

---
+++ conf/initUtil01.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /home/util01
+mkdir -p .config/zandronum/
+cp /app/util01/config/zandronum/zandronum.ini .config/zandronum/
+cp /app/util01/config/zandronum/freedoom2.wad .config/zandronum/
