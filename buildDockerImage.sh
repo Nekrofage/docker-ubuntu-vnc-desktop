721af1600fb62611b90d27a7e7aa1d7a10e4129e
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2019-02-03 00:08:41 +0100

    Update Build script

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-make
+REPO=nekrofage/ubuntu-desktop-lxde-vnc-zandronum                                                                
+TAG=latest
+IMAGE=ubuntu:18.04
+LOCALBUILD=1
+
+docker build -t $REPO:$TAG --build-arg localbuild=$LOCALBUILD --build-arg image=$IMAGE .
+
