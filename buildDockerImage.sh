145bccfd8bf228ebb233ff105597a00bbce20bf7
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2019-02-03 15:26:51 +0100

    Add Docker-compose

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -5,5 +5,4 @@ TAG=latest
 IMAGE=ubuntu:18.04
 LOCALBUILD=1
 
-docker build -t $REPO:$TAG --build-arg localbuild=$LOCALBUILD --build-arg image=$IMAGE .
-
+docker build -t $REPO:$TAG --build-arg localbuild=$LOCALBUILD --build-arg image=$IMAGE doomserver
--- generateSSL.sh
+++ generateSSL.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mkdir -p ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl/nginx.key -out ssl/nginx.crt
+mkdir -p doomserver/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout doomserver/ssl/nginx.key -out doomserver/ssl/nginx.crt
